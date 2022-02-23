2cf1aadffd867bb0c7776c4bfdefc9b8f8fa73a6
Miroslau <mira2408@mail.ru>
2022-02-23 19:44:12 +0300

    feat: add sign in logic

--- src/screens/user-screen/UserScreen.js
+++ src/screens/user-screen/UserScreen.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { ActivityIndicator } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { clearState, userSelector } from '../../store/slice/userSlice';
+import { loginUser, logoutUser } from '../../store/actions/userAction';
+import LoginScreen from './login-screen/LoginScreen';
+import ProfileScreen from './profile-screen/ProfileScreen';
 
-const UserScreen = () => (
-  <View>
-    <Text>User Screen</Text>
-  </View>
-);
+const UserScreen = () => {
+  const dispatch = useDispatch();
+  const { isSuccess, token } = useSelector(userSelector);
+
+  const signInUser = (user) => {
+    dispatch(loginUser(user));
+  };
+
+  const logOutUser = () => dispatch(logoutUser());
+
+  useEffect(() => () => {
+    dispatch(clearState());
+  }, []);
+
+  useEffect(() => {
+    if (isSuccess) {
+      dispatch(clearState());
+    }
+  }, [isSuccess]);
+
+  return (
+    <>
+      {
+        token ? <ProfileScreen logOutUser={logOutUser} /> : <LoginScreen signInUser={signInUser} />
+      }
+    </>
+  );
+};
 
 export default UserScreen;
---
+++ src/screens/user-screen/UserScreenStyle.js
@@ -0,0 +1,113 @@
+import { StyleSheet } from 'react-native';
+import { colorVariables } from '../../constants/colorVariables';
+
+export const UserScreenStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 25,
+    backgroundColor: colorVariables.colorWhite,
+  },
+  innerContainer: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+  },
+  logoTitle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  pageLogo: {
+    width: 50,
+    height: 50,
+  },
+  pageTitle: {
+    fontSize: 30,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: colorVariables.colorPurpleHeart,
+    padding: 10,
+  },
+  subTitle: {
+    fontSize: 20,
+    marginBottom: 20,
+    letterSpacing: 1,
+    fontWeight: 'bold',
+    color: colorVariables.colorEbonyClay,
+  },
+  styledFormArea: {
+    width: '90%',
+  },
+  styledTextInput: {
+    backgroundColor: colorVariables.colorAthensGray,
+    padding: 15,
+    paddingLeft: 55,
+    paddingRight: 55,
+    borderRadius: 5,
+    fontSize: 16,
+    height: 60,
+    marginVertical: 3,
+    marginBottom: 10,
+    color: colorVariables.colorEbonyClay,
+  },
+  errorInput: {
+    backgroundColor: 'transparent',
+    borderColor: colorVariables.colorFlamingo,
+    borderWidth: 1,
+    padding: 15,
+    paddingLeft: 55,
+    paddingRight: 55,
+    borderRadius: 5,
+    fontSize: 16,
+    height: 60,
+    marginVertical: 3,
+    marginBottom: 10,
+    color: colorVariables.colorFlamingo,
+  },
+  styledInputLabel: {
+    color: colorVariables.colorEbonyClay,
+    fontSize: 13,
+    textAlign: 'left',
+  },
+  leftIcon: {
+    left: 15,
+    top: 38,
+    position: 'absolute',
+    zIndex: 1,
+  },
+  icon: {
+    width: 30,
+    height: 30,
+  },
+  // Touchable
+  rightIcon: {
+    left: 15,
+    top: 38,
+    position: 'absolute',
+    zIndex: 1,
+  },
+  // Touchable
+  styledButton: {
+    padding: 15,
+    backgroundColor: colorVariables.colorPurpleHeart,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    marginVertical: 5,
+    height: 60,
+  },
+  buttonText: {
+    color: colorVariables.colorWhite,
+    fontSize: 16,
+  },
+  msgBox: {
+    textAlign: 'center',
+    fontSize: 13,
+  },
+  line: {
+    height: 1,
+    width: '100%',
+    backgroundColor: colorVariables.colorEbonyClay,
+    marginVertical: 10,
+  },
+});
