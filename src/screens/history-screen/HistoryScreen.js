f1fb6e7f1dba6c7f737a127b358e4f3b381214d2
Miroslau <mira2408@mail.ru>
2022-02-24 13:37:09 +0300

    feat: created visit history screen and trip screen

---
+++ src/screens/history-screen/HistoryScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HistoryScreen = () => (
+  <View>
+    <Text>History screen</Text>
+  </View>
+);
+
+export default HistoryScreen;
---
+++ src/screens/upcoming-trips-screen/UpcomingTripsScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const UpcomingTripsScreen = () => (
+  <View>
+    <Text>UpcomingTripsScreen</Text>
+  </View>
+);
+
+export default UpcomingTripsScreen;
