b3d299226c14b0a61fa7ad98f974bf1d544b3d79
Miroslau <mira2408@mail.ru>
2022-02-24 16:31:55 +0300

    feat: fix style

--- src/components/apartment-card/ApartmentCardStyle.js
+++ src/components/apartment-card/ApartmentCardStyle.js
@@ -9,6 +9,7 @@ const apartmentCardStyle = StyleSheet.create({
     backgroundColor: colorVariables.colorWhite,
     borderTopLeftRadius: 5,
     borderTopRightRadius: 5,
+    marginHorizontal: 10,
     marginVertical: 10,
     shadowColor: colorVariables.colorBlack,
     shadowRadius: 5,
